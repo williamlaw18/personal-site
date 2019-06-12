40e45894cc8a5db57f1bd86900123dd89d48d3d9
Will <will@pixeljunction.co.uk>
2019-06-12 14:24:17 +0100

    Setup project structure

---
+++ src/components/organisms/hero.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+const Hero = () => {
+    return(
+
+        <div className="page-hero">
+            <div className="container">
+        
+            <h1>Title Here</h1>
+            <h2>Description here</h2>
+        
+            </div>
+        </div>
+    )
+}
+
+export default Hero
