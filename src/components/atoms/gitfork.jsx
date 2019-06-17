4aaaf79e6f6a3bf4d7c062af72dcd9ced68b2db0
Will <will@pixeljunction.co.uk>
2019-06-17 16:22:36 +0100

    Styling tweaks & added toggle to cta

--- src/components/atoms/gitfork.jsx
+++ src/components/atoms/gitfork.jsx
@@ -6,7 +6,7 @@ const Gitfork = () => {
 
         <div className="container__gitfork">
 
-            <a href="https://github.com/williamlaw18"><img width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_green_007200.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1"></img></a>        
+            <a href="https://github.com/williamlaw18"><img width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_green_007200.png?resize=149%2C149" className="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1"></img></a>        
        
         </div>
 
--- src/components/molecules/cta-single.jsx
+++ src/components/molecules/cta-single.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 
 class CtaSingle extends Component{
+
+    state = {
+        ctaToggle: false,
+        ctaClass: ''
+    }
+
+    handleClick = (ctaToggle) => {
+        this.setState({
+            ctaToggle: !this.state.ctaToggle
+        })
+    }
+
     render(){
         return(
 
-            <div className="cta cta__single">
+            <div className="cta cta__single" toggled={this.state.ctaToggle.toString()}>
                 
-                <div className="cta__single-wrapper" style={{backgroundImage: "url("+ this.props.image +")"}}>
+                <div className={"cta__single-wrapper" + this.state.ctaClass} onClick={this.handleClick} style={{backgroundImage: "url("+ this.props.image +")"}}>
 
                     <div className="cta__single-preview">
                         <h3>{this.props.preview}</h3>
