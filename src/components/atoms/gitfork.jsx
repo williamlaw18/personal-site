b58c757a19eb99508636777bbf9b29e2bb2aa42b
Will Law <45406727+Will-Law@users.noreply.github.com>
2019-06-17 22:53:32 +0100

    Changed colors and fixed bugs

--- src/components/atoms/gitfork.jsx
+++ src/components/atoms/gitfork.jsx
@@ -6,8 +6,8 @@ const Gitfork = () => {
 
         <div className="container__gitfork">
 
-            <a href="https://github.com/williamlaw18"><img width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_green_007200.png?resize=149%2C149" className="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1"></img></a>        
-       
+            <a href="https://github.com/williamlaw18"><img width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_red_aa0000.png?resize=149%2C149" className="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1"></img></a>        
+
         </div>
 
     )
---
+++ src/components/organisms/footer.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Tagline from '../molecules/tagline';
+
+class Footer extends Component{
+
+    render(){
+        return(
+
+            <footer>
+
+                <section className="container container__footer">
+
+                  <h3>Footer Content</h3>
+
+                </section>
+
+            </footer>
+        )
+    }
+}
+
+export default Footer
