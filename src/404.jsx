0cf047ead0b2a4389f20d69186b42432e5e7ce72
Will Law <41906067+williamlaw18@users.noreply.github.com>
2022-05-15 01:13:36 +0100

    Assigned key values to component props

--- src/404.jsx
+++ src/404.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import './styles/base/base.scss';
@@ -10,18 +10,31 @@ import './styles/components/badPage.scss';
 const BadPage = () => {
 
   const container = useRef(null)
+
+  const [canvasType, setCanvasType] = useState('hex');
+
+  useEffect(() => {
+    console.log(canvasType)
+  }, [canvasType])
+
   return(
 
       <React.Fragment>
         <main className='badPage' ref={container}>
 
-            <Canvas type={'hex'} mouse={true} container={container}/>
+          <Canvas 
+            type={canvasType}
+            mouse={true}
+            container={container}
+            />
 
             <section className='pagecontainer'>
               <div className="page">
                 <h1>Oops! 404</h1>
                 <h2>Missing page</h2>
                 <Link className='button' to={'/'}>Take me back!</Link>
+                
+                <button className='button' onClick={() => setCanvasType('circle')}> Change!</button>
               </div>
             </section>
         </main>
--- src/components/canvas.jsx
+++ src/components/canvas.jsx
@@ -1,11 +1,21 @@
 import React, { useEffect, useRef, useState } from "react";
 
-const Canvas = ({type, mouse, loading, container}) => {
+const Canvas = (
+    {loading, container, type, mouse, rate = 500, colors = [], border = true, minRadius = 10, maxRadius = 100, xMove = 1.5, yMove = 1,}) => {
+    
     const canvasRef = useRef()
     const objects = useRef([])
+
+    const animateRef = useRef();
+    const interval = useRef(null)
     
     const [canvasHeight, setCanvasHeight] = useState(0);
     const [canvasWidth, setCanvasWidth] = useState(0);
+
+    const initCanvas = () => {
+        setCanvasArea();
+        resetInterval()
+    }
 
     const setCanvasArea = () => {
         if (container.current != null){
@@ -14,84 +24,99 @@ const Canvas = ({type, mouse, loading, container}) => {
         }
     }
 
+    //adds particle to list
     const createObject = (x, y) => {
         const object = {
             type: type,
             x: x,
             y: y,
-            radius: Math.floor(10 + (Math.random() * (100 - 10))) 
+            border: border,
+            color: colors[Math.floor((Math.random() * colors.length))],
+            radius: Math.floor(minRadius + (Math.random() * (maxRadius - minRadius))),
         };
         objects.current = [...objects.current, object]
     }
 
+    //draws particles on canvas
     const drawObjects = () => {
-        const ctx = canvasRef.current?.getContext('2d')
-        ctx.clearRect(0, 0, canvasRef.current.offsetWidth, canvasRef.current.offsetHeight);
+        if (canvasRef.current != null){
+            const ctx = canvasRef.current?.getContext('2d')
+            ctx.clearRect(0, 0, canvasRef.current.offsetWidth, canvasRef.current.offsetHeight);
+            
+            objects.current = objects.current.filter((item) => item.radius > minRadius);
+            
+            objects.current.forEach((object) => {
+                object.radius = (Math.max(0, object.radius - (0.25)));
+                object.x = ((object.x).toFixed(1) - xMove);
+                object.y = ((object.y).toFixed(1) - yMove);
+                ctx.beginPath()
 
-        objects.current = objects.current.filter((item) => item.radius > 10);
-            
-        objects.current.forEach((object) => {
-            object.radius = (Math.max(0, object.radius - (0.25)));
-            object.x = ((object.x).toFixed(1) - 0.5);
-            object.y = ((object.y).toFixed(1) - 1);
-            ctx.beginPath()
-            if (object.type == 'circle'){
-                ctx.arc(object.x, object.y, object.radius, 0, 2 * Math.PI, false)
-                ctx.fillStyle = "#B4E4FF"
-                ctx.fill()
-            } else if (object.type == 'hex'){
-                let a = 2 * (Math.PI) / 6;
-                for (let i = 0; i < 6; i++) {
-                    let posA = (object.x + object.radius * Math.cos(a * i))
-                    let posB = (object.y + object.radius * Math.sin(a * i))
-                    ctx.lineTo(posA.toFixed(1), posB.toFixed(1));
+                if (object.type == 'circle'){
+                    ctx.arc(object.x, object.y, object.radius, 0, 2 * Math.PI, false)
+                } else if (object.type == 'hex'){
+                    let a = 2 * (Math.PI) / 6;
+                    for (let i = 0; i < 6; i++) {
+                        let posA = (object.x + object.radius * Math.cos(a * i))
+                        let posB = (object.y + object.radius * Math.sin(a * i))
+                        ctx.lineTo(posA.toFixed(1), posB.toFixed(1));
+                    }
                 }
-                ctx.closePath();
-                ctx.stroke();
-            }
-            
-        });
+                if (object.border) {
+                    ctx.closePath();
+                    ctx.stroke();
+                }
+                if(colors.length > 0){
+                    ctx.fillStyle = object.color;
+                    ctx.fill()
+                }
+            });
+        }
     };
 
+    //Creates particle randomly
     const randomObject = () => {
-        let randomX = 10 + Math.floor(Math.random() * (canvasRef.current.width - 10));
-        let randomY = 10 + Math.floor(Math.random() * (canvasRef.current.height - 10));
-        createObject(randomX, randomY);
+        if (canvasRef.current != null){
+            let randomX = 10 + Math.floor(Math.random() * (canvasRef.current.width - 10));
+            let randomY = 10 + Math.floor(Math.random() * (canvasRef.current.height - 10));
+            createObject(randomX, randomY);
+        }
         drawObjects();
     }
 
+    //Creates particle on cursor pos
     const canvasDraw = (e) => {
         const canDraw = (e.nativeEvent.offsetX * e.nativeEvent.offsetY) % 10 == 0
         if (canDraw && mouse == true) createObject(Math.floor(e.nativeEvent.offsetX), Math.floor(e.nativeEvent.offsetY));
         if (mouse == true) drawObjects();
     };
-    
-    const requestRef = useRef();
 
+    //Anim frames
     const animate = () => {
         drawObjects();
-        requestRef.current = requestAnimationFrame(animate);
+        animateRef.current = requestAnimationFrame(animate);
+    }
+
+    const resetInterval = () => {
+        if (interval.current != null) clearInterval(interval.current);
+        interval.current = setInterval(() => {
+            randomObject();
+        }, rate)
     }
     
+    //Component update
     useEffect(() => {     
-    
-        const addObject = setInterval(() => {
-            randomObject();
-        }, 500)
-                
         if (loading == false || loading == null){
-            setCanvasArea();
-
-            requestRef.current = requestAnimationFrame(animate);
-            return () => cancelAnimationFrame(requestRef.current);
+            initCanvas()
+            animateRef.current = requestAnimationFrame(animate);
+            return () => cancelAnimationFrame(animateRef.current);
         }
         window.addEventListener('resize', setCanvasArea);
 
         return () => {
-            clearInterval(addObject)
+            clearInterval(interval.current);
             window.removeEventListener('resize', setCanvasArea);
         };
-    }, [loading]);
+    }, [loading, type]);
 
     return (
         <React.Fragment>
