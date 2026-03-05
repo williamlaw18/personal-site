3c5ff2228804ddb0333f948da04746c7053b465d
Will Law <greenodd.monk.e@gmail.com>
2026-03-06 01:32:53 +1100

    Added img carousel

--- src/app/components/PortfolioTile.js
+++ src/app/components/PortfolioTile.js
@@ -1,51 +1,82 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './PortfolioTile.module.css';
+import ScreenshotCarousel from './ScreenshotCarousel';
 
-export default function PortfolioTile({ title, description, tech, highlights, icon, screenshots = [], link, expanded, onClick, animationDelay = '0s' }) {
+export default function PortfolioTile({
+  title,
+  description,
+  tech,
+  highlights,
+  icon,
+  screenshots = [],
+  link,
+  expanded,
+  role,
+  onClick,
+  desc_extra,
+  detail,
+  animationDelay = '0s'
+}) {
   return (
     <div
       className={`${styles.tile} ${expanded ? styles.expanded : ''} ${styles.fadeSlideUp}`}
       style={{ animationDelay }}
       onClick={onClick}
     >
-    <div className={styles.titleContainer}>
-    <h3 className={styles.tileTitle}>{title}</h3>
-    {icon && (
-    <img src={icon} alt={`${title} icon`} className={styles.tileIcon} />
-    )}
-    </div>
-    <p className={styles.tileDescription}>{description}</p>
-
-    {expanded && (
-        <div className={styles.details}>
-            {screenshots.length > 0 && (
-            <div className={styles.screenshotContainer}>
-                {screenshots.map((src, index) => (
-                <img
-                    key={index}
-                    src={src}
-                    alt={`${title} screenshot ${index + 1}`}
-                    className={styles.screenshot}
-                />
-                ))}
-            </div>
-            )}
-
-            <div className={styles.infoSections}>
-            <p><strong>Tech Stack: </strong>{tech}</p>
-            <p><strong>Role: </strong>Full Stack Developer</p>
-            <p><strong>Highlights: </strong>{highlights}</p>
-            </div>
-
-            {link && (
-            <a href={link} className={styles.tileLink} target="_blank" rel="noopener noreferrer">
-                Visit Project
-            </a>
-            )}
-        </div>
+      <div className={styles.titleContainer}>
+        <h3 className={styles.tileTitle}>{title}</h3>
+        {icon && (
+          <img src={icon} alt={`${title} icon`} className={styles.tileIcon} />
         )}
+      </div>
+
+      <p className={styles.tileDescription}>{description}</p>
+
+      {expanded && (
+        <>
+        <p className={styles.fadeSlideUp}>{desc_extra}</p>
+        <div className={styles.details} onClick={(e) => e.stopPropagation()}>
+          
+          <p className={styles.content}>
+            {detail}
+          </p>
+          {screenshots.length > 0 && (
+            <div
+              className={styles.screenshotContainer}
+              onClick={(e) => e.stopPropagation()}
+            >
+              {screenshots.length > 0 && (
+                <ScreenshotCarousel screenshots={screenshots} />
+              )}
+            </div>
+          )}
+
+          <div className={styles.infoSections}>
+            <p><strong>Role: </strong>{role}</p>
+            <p><strong>Tech Stack: </strong>{tech}</p>
+            <p><strong>Highlights: </strong>{highlights}</p>
+          </div>
+
+          {link && (
+            <a
+              href={link}
+              className={styles.tileLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={(e) => e.stopPropagation()}
+            >
+              Visit Project
+            </a>
+          )}
+
+          <div>
+
+          </div>
+        </div>
+        </>
+      )}
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -10,28 +10,41 @@ const portfolioItemsData = [
   {
     id: 1,
     title: 'FretFocus',
-    description: 'A music learning platform designed to teach guitar and music theory interactively.',
+    description: 'A subscription-based music learning platform, containing cloud hosted multi-angled tutorials within structured course content.',
+    desc_extra: 'This project was a lot of fun! It was entirely, planned, designed, developed, and tested by myself on behalf of the Fitzpatrick Academy of Music team.',
     screenshots: [
-      '/screenshots/fretfocus-1.png',
-      '/screenshots/fretfocus-2.png',
+      '/site_imgs/ff_1.png',
+      '/site_imgs/ff_2.png',
+      '/site_imgs/ff_3.png',
     ],
     tech: 'React js, Next.js, Node.js, StrapiCMS, Stripe Payments, AWS Backend, Vercel Headless',
     highlights: 'Video Streaming, Video Encoding, Payment Subscription, Cloud Hosting',
+    role: 'Full Stack Developer, Designer, Architect',
     link: 'https://fretfocus.com/',
-    icon: './icons/ff_logo.svg'
+    icon: './icons/ff_logo.svg',
+    detail: `The site is built using the JAM stack architecture. It consists of a Next.js frontend which serves as a platform for customers to interact with lessons. This retreives the lesson content via the backend API. \n
+    The backend is hosted on an AWS instance and uses StrapiCMS, covering content management, user authentication, and facilitates the ability to upload videos into cloud storage. It also handles communication with Stripe for payment processing and user verification \n
+    Uploaded videos are encoded inside of AWS and are stored within streamable packets on an S3 bucket. These are then delivered to the frontend via a cloudfront URL served from StrapiCMS \n`
   },
   {
     id: 2,
     title: 'Study Buddy',
-    description: 'A learning chatbot to help students study efficiently with AI-powered guidance.',
+    description: 'A researched focused exploration into AI chatbots in education. Using an iteratively fine-tuned GPT model',
+    desc_extra: 'This project was conducted as part of my undergraduate dissertation. It focuses on testing the potential that large language models exhibit for distilling and explaining information within an educational context.',
+    detail: `Towards the end of my studies, tools like ChatGPT were released to the public. During this time I saw the profound impact it had students ability to learn, but also their ability to cheat. The whole education sector felt this impact and jumped immidately to fear and dismissal of these systems. \n
+    My own experience however, was shaped through curiosity and respect for the system to be viewed as a tool. The ability to have deep intelectual back and fourth, distill topics and pull together notes without waiting 24hours for a lecturer's email, was unmatched and unprecedented \n
+    Therefore it felt nessesary to explore the potential educational possibilites of such a tool, and if such a tool could be refined and placed within contextual guard-rails, so that it fit within an educational setting.  \n
+    A custom GPT model was refined using cloud compute, and trained using a combination of real-world and synthetic datasets. The trained model is hosted within GPT labs and called via an API upon chat submission from a basic React.js skeleton.`,
     screenshots: [
-      '/screenshots/studybuddy-1.png',
-      '/screenshots/studybuddy-2.png',
+      '/site_imgs/sb_3.png',
+      '/site_imgs/sb_1.png',
+      '/site_imgs/sb_2.png'
     ],
     tech: 'React js, Netlify Headless, Custom GPT model',
     highlights: 'Interactive Chatbot, AI Powered Model',
+    role: 'Developer, Researcher',
     link: 'https://studybuddy-tool.netlify.app/',
-    icon: './icons/sb_logo.svg'
+    icon: './icons/sb_logo.svg',
   },
 ];
 
@@ -55,7 +68,7 @@ export default function Page() {
         style={{ animationDelay: '0s' }}
       >
         <h1>William Law 👋</h1>
-        <p>Motivated Web developer | React & Next.js | Industry Experienced</p>
+        <p>Web developer | React & Next.js | Industry Experienced</p>
       </header>
 
       <section
@@ -74,7 +87,10 @@ export default function Page() {
               expanded={item.id === expandedId}
               tech={item.tech}
               highlights={item.highlights}
+              role={item.role}
               icon={item.icon}
+              detail={item.detail}
+              desc_extra={item.desc_extra}
               onClick={() => setExpandedId(item.id === expandedId ? null : item.id)}
               animationDelay={`${0.15 * index + 0.2}s`} // stagger tiles
             />
